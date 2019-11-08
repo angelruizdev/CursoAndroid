2420753079634ffcb258839058bb250d33d13822
angelruiz <angeldavid29121990@gmail.com>
2019-11-07 21:32:54 -0600

    Working in content provider in their methods query and insert, for save and show information of weight ussing the class ContentUris.

--- app/src/main/java/com/example/angelruiz/cursoandroid/CustomContentPovider/ModelContentProvider/WeightProvider.java
+++ app/src/main/java/com/example/angelruiz/cursoandroid/CustomContentPovider/ModelContentProvider/WeightProvider.java
@@ -1,12 +1,15 @@
 package com.example.angelruiz.cursoandroid.CustomContentPovider.ModelContentProvider;
 
 import android.content.ContentProvider;
+import android.content.ContentUris;
 import android.content.ContentValues;
 import android.content.Context;
 import android.content.UriMatcher;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.net.Uri;
+import android.util.Log;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -14,7 +17,7 @@ import androidx.annotation.Nullable;
 public class WeightProvider extends ContentProvider {
     Context context;
     SQLiteDatabase sqLiteDatabase;
-    private DataBaseCPOpnHpr dataBaseCPOpnHpr;
+    DataBaseCPOpnHpr dataBaseCPOpnHpr;
 
     //uri matcher code for the table of peso
     private static final int PESO_PERSONA = 0;
@@ -36,32 +39,36 @@ public class WeightProvider extends ContentProvider {
     public boolean onCreate() {
         context = getContext();
         dataBaseCPOpnHpr = new DataBaseCPOpnHpr(context, ContractSqliteConstantsCP.ConstantsSqliteDB.NAME_DATABASE_CP, null, ContractSqliteConstantsCP.ConstantsSqliteDB.VERSION_DATABASE);
-
+        sqLiteDatabase = dataBaseCPOpnHpr.getReadableDatabase();
         return true;
     }
 
-    //we open the db in read mode
+    //we open the db in read mode for show registers
     @Nullable
     @Override
     public Cursor query(@NonNull Uri uri, @Nullable String[] projection, @Nullable String selection, @Nullable String[] selectionArgs, @Nullable String sortOrder) {
 
-        sqLiteDatabase = dataBaseCPOpnHpr.getReadableDatabase();
         Cursor cursor;
 
         int macth = uriMatcher.match(uri);
         switch (macth){
             case PESO_PERSONA:
+                //consult all registers
                 cursor = sqLiteDatabase.query(ContractSqliteConstantsCP.ConstantsSqliteDB.NAME_TABLE, projection, selection, selectionArgs, null, null, sortOrder);
                 cursor.setNotificationUri(context.getContentResolver(), ContractSqliteConstantsCP.ConstantsSqliteDB.CONTENT_URI);
-                cursor.close();
                 break;
 
             case PESO_ID:
-
+                //consult a only register based in the id of uri
+                selection = ContractSqliteConstantsCP.ConstantsSqliteDB._ID + "=?";
+                selectionArgs = new String[]{String.valueOf(ContentUris.parseId(uri))};
+                cursor = sqLiteDatabase.query(ContractSqliteConstantsCP.ConstantsSqliteDB.NAME_TABLE, projection, selection, selectionArgs, null, null, sortOrder);
+                cursor.setNotificationUri(context.getContentResolver(), ContractSqliteConstantsCP.ConstantsSqliteDB.CONTENT_URI);
                 break;
+
             default: throw new IllegalArgumentException("Unknow Uri" + uri);
         }
-        return sqLiteDatabase.query(ContractSqliteConstantsCP.ConstantsSqliteDB.NAME_TABLE, projection, selection, selectionArgs, null, null, sortOrder);
+        return cursor;
     }
 
     @Nullable
@@ -71,10 +78,38 @@ public class WeightProvider extends ContentProvider {
         return null;
     }
 
+
     @Nullable
     @Override
     public Uri insert(@NonNull Uri uri, @Nullable ContentValues contentValues) {
+
+        //check which code of uri will use
+        int match1 = uriMatcher.match(uri);
+          switch (match1){
+              case PESO_PERSONA:
+
+                 return insertWeight(uri, contentValues);
+
+          default: throw new IllegalArgumentException("\n" + "invalid data" + uri);
+          }
+    }
+
+    //save data in CPSqlite
+    private Uri insertWeight(Uri uri, ContentValues contentValues){
+
+        //save the the insert in a long
+        long id = sqLiteDatabase.insert(ContractSqliteConstantsCP.ConstantsSqliteDB.NAME_TABLE, null, contentValues);
+
+        //if insert return -1 data not saved
+        if (id == -1){
+            Toast.makeText(context, "¡Error al guardar datos!", Toast.LENGTH_SHORT).show();
+            Log.i("ErrorSaveData", "data not saved");
         return null;
+
+        }else{
+            Log.i("SaveData", "data saved " + ContentUris.withAppendedId(uri, id));
+        }
+        return ContentUris.withAppendedId(uri, id); // join the uri own with the id(insert) for save it
     }
 
     @Override
--- app/src/main/java/com/example/angelruiz/cursoandroid/Fragments/FragmentWeightCPBNV.java
+++ app/src/main/java/com/example/angelruiz/cursoandroid/Fragments/FragmentWeightCPBNV.java
@@ -3,18 +3,18 @@ package com.example.angelruiz.cursoandroid.Fragments;
 import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
 import android.icu.text.SimpleDateFormat;
 import android.icu.util.Calendar;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.annotation.RequiresApi;
@@ -22,16 +22,16 @@ import androidx.fragment.app.Fragment;
 
 import com.example.angelruiz.cursoandroid.CustomContentPovider.ModelContentProvider.ContractSqliteConstantsCP;
 import com.example.angelruiz.cursoandroid.R;
-
+//the objects comented were to access to the db directly without use CP
 public class FragmentWeightCPBNV extends Fragment {
     View view;
     private EditText etWeigthEnter;
     private TextView tvShowResults;
     private Button btSaveRegisterCP;
-    //private DataBaseCPOpnHpr connection;
+    //private DataBaseCPOpnHpr connection; we coment this object for use Cursor and content resolver for access to the data
     Context context;
     private String dateCurrent;
-    private SQLiteDatabase sqLiteDatabase;
+    //private SQLiteDatabase sqLiteDatabase;
 
     public FragmentWeightCPBNV() {
         // Required empty public constructor
@@ -69,6 +69,7 @@ public class FragmentWeightCPBNV extends Fragment {
         });
     }
 
+    //obtain date for save register
     @RequiresApi(api = Build.VERSION_CODES.N)
     private String obtainDateDevice(){
 
@@ -82,6 +83,7 @@ public class FragmentWeightCPBNV extends Fragment {
         return obtainDateCurrent;
     }
 
+    //insert data in Sqlite CP
     @RequiresApi(api = Build.VERSION_CODES.N)
     private void saveInfoWeight() {
         //sqLiteDatabase = connection.getWritableDatabase();
@@ -94,12 +96,16 @@ public class FragmentWeightCPBNV extends Fragment {
            values.put(ContractSqliteConstantsCP.ConstantsSqliteDB.COLUMN_PESO, peso);
            values.put(ContractSqliteConstantsCP.ConstantsSqliteDB.COLUMN_DATE, dateCurrent);
 
-           sqLiteDatabase.insert(ContractSqliteConstantsCP.ConstantsSqliteDB.NAME_TABLE, null, values);
+           //sqLiteDatabase.insert(ContractSqliteConstantsCP.ConstantsSqliteDB.NAME_TABLE, null, values);
+           //sqLiteDatabase.close();
+
+           //this object its communicate with method insert of WeightProvider
+           Uri newUriInsert = context.getContentResolver().insert(ContractSqliteConstantsCP.ConstantsSqliteDB.CONTENT_URI, values);
            etWeigthEnter.setText("");
-           sqLiteDatabase.close();
         }
     }
 
+    //show registers of Sqlite CP
     private void showInfoWeight(){
         //sqLiteDatabase = connection.getReadableDatabase();
         //Cursor cursor = sqLiteDatabase.rawQuery("SELECT * FROM " + ContractSqliteConstantsCP.ConstantsSqliteDB.NAME_TABLE, null);
@@ -107,16 +113,22 @@ public class FragmentWeightCPBNV extends Fragment {
                                ContractSqliteConstantsCP.ConstantsSqliteDB.COLUMN_PESO,
                                ContractSqliteConstantsCP.ConstantsSqliteDB.COLUMN_DATE};
 
+        //this object its communicate with method query of WeightProvider
         Cursor cursor = context.getContentResolver().query(ContractSqliteConstantsCP.ConstantsSqliteDB.CONTENT_URI, projection, null, null, null);
+        String idPerson, weight, date;
+
         if (cursor != null){
             while (cursor.moveToNext()){
-                String w = cursor.getString(1);
-                String d = cursor.getString(2);
-                tvShowResults.append("number of raws: "+"\n" + cursor.getCount() + "---" + w +"-"+ d );
+
+                idPerson = cursor.getString(0);
+                weight = cursor.getString(1);
+                date = cursor.getString(2);
+
+                tvShowResults.append("\n" + "number of raws: "+ cursor.getCount() +"\n" + idPerson +"-"+ weight +"-"+ date );
             }
             cursor.close();
         }else {
-            Toast.makeText(context, "null", Toast.LENGTH_SHORT).show();
+            Log.i("cursor", "cursor null");
         }
     }
 
